919114fda411541af532ffa714030848b3b766f3
Ashton Knoblock <Ashton.knoblock@gmail.com>
2018-10-02 10:00:38 -0400

    added startedbot.py

--- starterbot.py
+++ starterbot.py
@@ -20,8 +20,38 @@ MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 running_flag = True
 help_count = 0
 # Get random jokes from jokes.py...
-# NOT SURE if it will work if someone else tried dl-ing
-joke = jokes.jokes
+# NOT SURE if it will work if someone else tried dl-ingv 
+joke = [
+    '''I asked God for a bike, but I know God doesn’t work that way.
+ So I stole a bike and asked for forgiveness.''',
+    '''Do not argue with an idiot.
+ He will drag you down to his level and beat you with experience.''',
+    '''I want to die peacefully in my sleep, like my grandfather..
+ Not screaming and yelling like the passengers in his car.''',
+    'The last thing I want to do is hurt you. But it’s still on the list.',
+    '''Light travels faster than sound.
+     This is why some people appear bright until you hear them speak.''',
+    'If I agreed with you we’d both be wrong.',
+    'The early bird might get the worm, but the second mouse gets the cheese.',
+   '''Politicians and diapers have one thing in common.
+ They should both be changed regularly, and for the same reason.''',
+    '''A bus station is where a bus stops. A train station is where a train stops.
+ On my desk, I have a work station..''',
+    '''Some people are like Slinkies … not really good for anything,
+ but you can’t help smiling when you see one tumble down the stairs.''',
+    ''''How is it one careless match can start a forest fire,
+ but it takes a whole box to start a campfire?''',
+    '''To steal ideas from one person is plagiarism.
+ To steal from many is research.''',
+    'Artificial intelligence is no match for natural stupidity.',
+    'God must love stupid people. He made SO many.',
+    'My opinions may have changed, but not the fact that I am right.',
+    '''You do not need a parachute to skydive.
+ You only need a parachute to skydive twice.''',
+    'Nostalgia isn’t what it used to be.',
+    'intend to live forever. So far, so good.',
+    'With sufficient thrust, pigs fly just fine'
+    ]
 
 
 # setup the logger
